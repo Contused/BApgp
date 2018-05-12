38e436c068c2fc6c5245494789efc0b87bae83eb
Contused <alex.ownd@gmx.de>
2018-05-12 17:57:36 +0200

    [NEU] Hintergrundfarben werden angezeigt und mit der Json abgeglichen.

--- Extension/src/pguard.js
+++ Extension/src/pguard.js
@@ -1,6 +1,19 @@
 /**
  * Created by Alexander on 03.05.2018.
  */
+
+function isRedLine(id){
+    $.getJSON(chrome.extension.getURL("lib/data/IB_texte.json"), function(texte) {
+        for(var i = 0; i < texte.length; i++){
+            if(id === texte[i].id && texte[i].is_red_line === "true"){
+                console.log("RED");
+                return true;
+            }
+        }
+        return false;
+    });
+}
+
 function createTextElement(appSquare){
     var appID = $(appSquare).attr("data-docid");
     if(appID){
@@ -16,11 +29,18 @@ function createTextElement(appSquare){
 
                 var testSpan = document.createElement('span');
                 if(data[0]){
+                    frame.style.backgroundColor = "#adccff";
+                    for(var i = 0; i < data[0].infoboxes.length; i++){
+                        var redLine = isRedLine(data[0].infoboxes[i].id);
+                        if(redLine){
+                            frame.style.backgroundColor = "#d85443";
+                            break;
+                        }
+                    }
                     testSpan.textContent = "Funde: " + data[0].infoboxes.length;
                 } else {
-                    testSpan.textContent = "Funde: 0";
+                    testSpan.textContent = "Ergebnisse anzeigen";
                 }
-
                 frame.appendChild(testSpan);
 
                 appSquare.insertBefore(frame,appSquare.childNodes[0]);
