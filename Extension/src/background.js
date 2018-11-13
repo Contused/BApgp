a7fddb695018717027c46d29aaf915242559ca66
Contused <alex.pr@gmx.de>
2018-11-13 15:30:11 +0100

    [NEU] Programmaufbau - Background.js geschrieben.

--- Extension/src/background.js
+++ Extension/src/background.js
@@ -1,6 +1,8 @@
+/**
+ * Created by Alexander on 04.04.2018.
+ */
 chrome.runtime.onInstalled.addListener(function() {
-    console.log('jup');
-    //removeRules, braucht declarativeContent permission, 1 = liste an regeln(undefined = nix), 2= function nach löschen
+    //removeRules, braucht declarativeContent permission, 1 = liste an regeln(undefined = keine Eingabe), 2= function nach loeschen
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         //siehe removeRules, wird am ende von remove rules aufgerufen.
         chrome.declarativeContent.onPageChanged.addRules([{
@@ -11,6 +13,4 @@ chrome.runtime.onInstalled.addListener(function() {
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
     });
-});/**
- * Created by Alexander on 04.04.2018.
- */
+});
